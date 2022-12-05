cdb41ac1a9996eb3c2b8cb49bd47a1d1c5358b31
cubesteam <95546870+cubesteam@users.noreply.github.com>
2022-12-05 08:18:33 +0200

    sale_date
    sale_date

---
+++ sale_date/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Cubes Sale Order Date",
+
+    'summary': """
+        Cubes Sale Order Date
+        """,
+
+    'description': """
+        Made By Abdulwahed Frea 2022-12-04
+    """,
+
+    'author': "Cubes  Company",
+    'website': "http://www.cubes.ly",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['sale','account'],
+
+    'data': [
+        'views/sale_order.xml',
+        'views/account_move.xml',
+        # 'views/sale_order.xml',
+    ],
+}
